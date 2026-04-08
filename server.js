783498a18518bab76e772e417794eda2871e5035
Valeria <6232300228@univermilenium.edu.mx>
2026-04-08 02:13:08 -0600

    correo

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ const mercadopagoClient = new MercadoPagoConfig({
     accessToken: process.env.MP_ACCESS_TOKEN || "APP_USR-5562521962692930-030522-9080c61c1567cf8b93f52eb8a9dfa477-3247325848"
 });
 
-app.use(express.json());
-
 app.use(cors({
     origin: [
         'https://luxecollection.org',
@@ -30,8 +28,13 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// Manejar explicitamente las peticiones OPTIONS
-app.options('*', cors());
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(200);
+});
 
 app.use(session({
     secret: process.env.SESSION_SECRET || 'luxe-secret-key',
